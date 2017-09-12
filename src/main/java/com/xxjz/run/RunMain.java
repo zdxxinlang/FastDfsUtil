3f2847205541b755ee5b0da10fb2b55d108dfee4
xxjz <zdxxinlang@sina.cn>
2017-09-12 18:30:35 +0800

    初次提交

---
+++ src/main/java/com/xxjz/run/RunMain.java
@@ -0,0 +1,20 @@
+package com.xxjz.run;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class RunMain {
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+		
+        return "Hello spring boot! ";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RunMain.class, args);
+    }
+}
---
+++ src/main/java/com/xxjz/upload/FastFdsUtil.java
@@ -0,0 +1,5 @@
+package com.xxjz.upload;
+
+public class FastFdsUtil {
+
+}
